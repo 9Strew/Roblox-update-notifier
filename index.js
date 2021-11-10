e5e75f3db95b992b6feacae46043da427abded54
9Strew <83422292+9Strew@users.noreply.github.com>
2021-11-11 00:02:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const express = require('express')
 const app = express()
 const axios = require('axios').default
-let dwebhook = ""
+let dwebhook = "webhook here"
 
 setInterval(async () => {
     const currentRoblox = await axios.get('http://setup.roblox.com/version')
@@ -10,14 +10,20 @@ setInterval(async () => {
     if (savedRoblox.version !== currentRoblox.data) {
         const { Webhook, MessageBuilder } = require('discord-webhook-node');
         const hook = new Webhook(dwebhook);
+
+		
         const embed = new MessageBuilder()
-        .addField('Roblox Updated',`${currentRoblox.data}`, true)
+        .setAuthor('Roblox Updated!', '', 'http://setup.roblox.com/version')
+        .addField('New Version',"```diff\n- " + currentRoblox.data + "``` \n", true)
+        .addField('Old Version',"```diff\n+ " + savedRoblox.version + "```")
         .setColor('#00b0f4')
         .setFooter('do not dm to staff for this topic')
         .setTimestamp();
+		
         hook.send(embed);
-       // hook.send("@everyone");
-        
+        hook.send("@everyone");
+		
+		
         console.log(`roblox updated ${currentRoblox.data}`)
         savedRoblox.version = currentRoblox.data
         fs.writeFileSync('currentroblox.json', JSON.stringify(savedRoblox, null, 4))
